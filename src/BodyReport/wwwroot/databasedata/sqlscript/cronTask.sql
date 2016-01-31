eb6f6d28316553580530b15139a8d2ae7bd25512
Thetyne <thetyne@live.fr>
2016-01-31 15:01:55 +0100

    Add cron SQL script #19
    Add delete users who haven't confirm their email.

---
+++ src/BodyReport/wwwroot/databasedata/sqlscript/cronTask.sql
@@ -0,0 +1,2 @@
+/*supprime les utilisateurs qui n'ont pas validé leur mail qui ont plus de 24 heures d'existence*/
+delete from public."AspNetUsers" where "EmailConfirmed"=false and ("RegistrationDate" = null or (now()::timestamp - "RegistrationDate" > '24 hours') );
