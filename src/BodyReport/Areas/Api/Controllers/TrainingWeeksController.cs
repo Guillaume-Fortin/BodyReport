e509843b83330f8a30c9c91430a8bbcb3afb218f
Thetyne <thetyne@live.fr>
2016-04-23 19:51:23 +0200

    Manage and Display Web Api Exception on mobile application
    #6 in mobile project

--- src/BodyReport/Areas/Api/Controllers/TrainingWeeksController.cs
+++ src/BodyReport/Areas/Api/Controllers/TrainingWeeksController.cs
@@ -11,6 +11,7 @@ using Microsoft.Data.Entity;
 using Message.WebApi;
 using BodyReport.Services;
 using BodyReport.Framework.Exceptions;
+using BodyReport.Resources;
 
 namespace BodyReport.Areas.Api.Controllers
 {
@@ -35,75 +36,107 @@ namespace BodyReport.Areas.Api.Controllers
         [HttpGet]
         public IActionResult Get(TrainingWeekKey trainingWeekKey)
         {
-            if (trainingWeekKey == null)
-                return HttpBadRequest();
-            var trainingWeek = _manager.GetTrainingWeek(trainingWeekKey, false);
-            return new HttpOkObjectResult(trainingWeek);
+            try
+            {
+                if (trainingWeekKey == null)
+                    return HttpBadRequest();
+                var trainingWeek = _manager.GetTrainingWeek(trainingWeekKey, false);
+                return new HttpOkObjectResult(trainingWeek);
+            }
+            catch(Exception exception)
+            {
+                return HttpBadRequest(new WebApiException("Error", exception));
+            }
         }
 
         // Get api/TrainingWeeks/Find
         [HttpGet]
-		public List<TrainingWeek> Find()
+		public IActionResult Find()
 		{
-			string userId = User.GetUserId ();
-			var searchCriteria = new TrainingWeekCriteria() { UserId = new StringCriteria() { EqualList = new List<string>() { userId } } };
-			return _manager.FindTrainingWeek (searchCriteria, false);
-		}
+            try
+            {
+			    string userId = User.GetUserId ();
+			    var searchCriteria = new TrainingWeekCriteria() { UserId = new StringCriteria() { EqualList = new List<string>() { userId } } };
+			    return new HttpOkObjectResult(_manager.FindTrainingWeek (searchCriteria, false));
+            }
+            catch (Exception exception)
+            {
+                return HttpBadRequest(new WebApiException("Error", exception));
+            }
+        }
 
         // Post api/TrainingWeeks/Create
         [HttpPost]
         public IActionResult Create([FromBody]TrainingWeek trainingWeek)
         {
-            if(trainingWeek == null && trainingWeek.UserId != User.GetUserId())
-                return HttpBadRequest("Oups!");
+            try
+            {
+                if(trainingWeek == null && trainingWeek.UserId != User.GetUserId())
+                    return HttpBadRequest();
             
-            var result = _manager.CreateTrainingWeek(trainingWeek);
-            return new HttpOkObjectResult(result);
+                var result = _manager.CreateTrainingWeek(trainingWeek);
+                return new HttpOkObjectResult(result);
+            }
+            catch (Exception exception)
+            {
+                return HttpBadRequest(new WebApiException("Error", exception));
+            }
         }
 
         // Post api/TrainingWeeks/Update
         [HttpPost]
         public IActionResult Update([FromBody]TrainingWeek trainingWeek)
 		{
-           // return HttpBadRequest("Oups!");
-            TrainingWeek result = new TrainingWeek();
-			if (trainingWeek.UserId == User.GetUserId ())
-			{
-				result = _manager.UpdateTrainingWeek(trainingWeek);
-			}
-			return new HttpOkObjectResult(result);
-		}
+            try
+            {
+			    if (trainingWeek == null || trainingWeek.UserId != User.GetUserId ())
+			        return HttpBadRequest();
+                var result = _manager.UpdateTrainingWeek(trainingWeek);
+			    return new HttpOkObjectResult(result);
+            }
+            catch (Exception exception)
+            {
+                return HttpBadRequest(new WebApiException("Error", exception));
+            }
+        }
 
         // Post api/TrainingWeeks/DeleteByKey
         [HttpPost]
         public IActionResult DeleteByKey([FromBody]TrainingWeekKey trainingWeekKey)
         {
-            if (trainingWeekKey == null)
-                return HttpBadRequest("Oups!");
-
-            if (trainingWeekKey.UserId != User.GetUserId() || string.IsNullOrWhiteSpace(trainingWeekKey.UserId) || trainingWeekKey.Year == 0 || trainingWeekKey.WeekOfYear == 0)
-                return HttpBadRequest("Oups!");
-            
-            var trainingWeekManager = new TrainingWeekManager(_dbContext);
-            var trainingWeek = trainingWeekManager.GetTrainingWeek(trainingWeekKey, false);
-            if (trainingWeek == null)
-                return HttpBadRequest("Oups!");
-
-            using (var transaction = _dbContext.Database.BeginTransaction())
+            try
             {
-                try
+                if (trainingWeekKey == null )
+                    return HttpBadRequest();
+            
+                if (trainingWeekKey.UserId != User.GetUserId() || string.IsNullOrWhiteSpace(trainingWeekKey.UserId) || trainingWeekKey.Year == 0 || trainingWeekKey.WeekOfYear == 0)
+                    return HttpBadRequest();
+            
+                var trainingWeekManager = new TrainingWeekManager(_dbContext);
+                var trainingWeek = trainingWeekManager.GetTrainingWeek(trainingWeekKey, false);
+                if (trainingWeek == null)
+                    return HttpNotFound();
+
+                using (var transaction = _dbContext.Database.BeginTransaction())
                 {
-                    trainingWeekManager.DeleteTrainingWeek(trainingWeek);
-                    transaction.Commit();
+                    try
+                    {
+                        trainingWeekManager.DeleteTrainingWeek(trainingWeek);
+                        transaction.Commit();
+                    }
+                    catch (Exception exception)
+                    {
+                        //_logger.LogCritical("Unable to delete training week", exception);
+                        transaction.Rollback();
+                        throw exception;
+                    }
                 }
-                catch (Exception exception)
-                {
-                    //_logger.LogCritical("Unable to delete training week", exception);
-                    transaction.Rollback();
-                    throw exception;
-                }
+                return new HttpOkResult();
             }
-            return new HttpOkResult();
+            catch (Exception exception)
+            {
+                return HttpBadRequest(new WebApiException("Error", exception));
+            }
         }
 
         // Post api/TrainingWeeks/Copy
@@ -111,7 +144,7 @@ namespace BodyReport.Areas.Api.Controllers
         public IActionResult Copy([FromBody]CopyTrainingWeek copyTrainingWeek)
         {
             if (copyTrainingWeek == null)
-                return HttpBadRequest("Oups!");
+                return HttpBadRequest();
 
             try
             {
@@ -122,9 +155,9 @@ namespace BodyReport.Areas.Api.Controllers
 
                 return new HttpOkObjectResult(trainingWeek);
             }
-            catch (ErrorException error)
+            catch (Exception exception) when (exception is ErrorException || exception is Exception)
             {
-                return HttpBadRequest(error.Message);
+                return HttpBadRequest(new WebApiException("Error", exception));
             }
         }
     }
---
+++ src/Message/WebApi/HttpException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Message.WebApi
+{
+    public class HttpException : Exception
+    {
+        public int Code { get; set; } = 0;
+
+        public HttpException(string message, Exception innerException=null) : base(message, innerException)
+        {
+        }
+
+        public HttpException(int code, string message, Exception innerException = null) : base(message, innerException)
+        {
+            Code = code;
+        }
+    }
+}
