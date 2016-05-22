4d4bc21202c1206b88953a2039d6270e83593722
Thetyne <thetyne@live.fr>
2016-05-22 17:09:09 +0200

    Migrate production environment to Asp.Net RC2 #37

--- src/BodyReport/Program.cs
+++ src/BodyReport/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -11,13 +13,21 @@ namespace BodyReport
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                             .AddEnvironmentVariables("")
+                             .Build();
+            var url = config["ASPNETCORE_URLS"] ?? "http://*:5000";
+            var env = config["ASPNETCORE_ENVIRONMENT"] ?? "Development";
+
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseUrls(url)
+                .UseEnvironment(env)
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
-
+            
             host.Run();
         }
     }
