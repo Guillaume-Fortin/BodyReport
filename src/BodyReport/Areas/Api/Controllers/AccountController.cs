4f52fc8a194e211a1c07864e46616340e56ac15a
Thetyne <thetyne@live.fr>
2016-07-01 08:31:12 +0200

    Unable to validate account with mail box #46

--- src/BodyReport/Areas/Api/Controllers/AccountController.cs
+++ src/BodyReport/Areas/Api/Controllers/AccountController.cs
@@ -182,7 +182,7 @@ namespace BodyReport.Areas.Api.Controllers
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=532713
                     // Send an email with this link
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: HttpContext.Request.Scheme);
+                    var callbackUrl = Url.Action("ConfirmEmail", "Account", new { area = "Site", userId = user.Id, code = code }, protocol: HttpContext.Request.Scheme);
                     await _emailSender.SendEmailAsync(registerAccount.Email, "Confirm your account",
                         "Please confirm your account by clicking this link: <a href=\"" + callbackUrl + "\">link</a>");
                     //await _signInManager.SignInAsync(user, isPersistent: false);
